c5223e83b770db6383ff99a65bd27029e710ff08
Ben Milnes <benjamin.milnes@nagwa.com>
2018-06-20 16:09:32 +0100

    Tidied up JS code

--- app.js
+++ app.js
@@ -2,90 +2,80 @@ var app = angular.module("RandomDistractorGenerator", []);
 
 app.controller("MainController", ["$scope", function MainController($scope) {
 
-  $scope.value = "10 m/s";
+  $scope.value = "100 m/s";
   $scope.spread = 0.5;
   $scope.numberOfDistractors = 4;
 
   $scope.distractors = [];
 
-
   $scope.randomNumberAroundValue = function(value, spread) {
-
     var absoluteSpread = value * spread;
 
     return value + 2 * absoluteSpread * Math.random() - absoluteSpread;
-
-
   }
 
   $scope.splitValueIntoNumberAndUnits = function(value) {
-
-    var splitPoint = -1;
+    var p = -1;
     var s = value.toString();
-
-    for (var i = 0; i < s.length; i++) {
-      c = s[i];
-      if ("0123456789.".indexOf(c) == -1) {
-        splitPoint = i;
-        break;
-      }
-    }
-
-    console.log(splitPoint);
-
-    return [s.slice(0, splitPoint), s.slice(splitPoint)];
-
-
-
-  }
-
-  $scope.getNumberOfSignificantFigures = function(number) {
-
-    var n = 0;
-    var h = false;
-    var hasHadPoint = false;
-    var s = number.toString();
 
     for (var i = 0; i < s.length; i++) {
       var c = s[i];
 
-      if (h == false) {
+      if ("0123456789.".indexOf(c) == -1) {
+        p = i;
+        break;
+      }
+    }
+
+    return [s.slice(0, p), s.slice(p)];
+  }
+
+  $scope.getNumberOfSignificantFigures = function(value) {
+    var n = 0;
+    var significantFiguresHaveStarted = false;
+    var haveHadDecimalPoint = false;
+    var s = value.toString();
+
+    for (var i = 0; i < s.length; i++) {
+      var c = s[i];
+
+      if (!significantFiguresHaveStarted) {
         if ("123456789".indexOf(c) != -1) {
-          h = true;
+          significantFiguresHaveStarted = true;
         } else {
           continue;
         }
       }
-      if (h == true) {
+
+      if (significantFiguresHaveStarted) {
         if (".".indexOf(c) == -1) {
           n++;
         }
-
       }
     }
 
     return n;
-
   }
-
 
   $scope.generateDistractors = function() {
 
-    var value2 = $scope.splitValueIntoNumberAndUnits($scope.value);
-    var v = parseFloat(value2[0])
-    var sf = parseInt($scope.getNumberOfSignificantFigures(value2[0].toString()));
+    var numberAndUnits = $scope.splitValueIntoNumberAndUnits($scope.value);
+    var number = parseFloat(numberAndUnits[0]);
+    var numberOfSignificantFigures = $scope.getNumberOfSignificantFigures(number);
+    var units = numberAndUnits[1];
 
     $scope.distractors = [];
 
     for (var i = 0; i < $scope.numberOfDistractors; i++) {
+      var distractor = $scope.randomNumberAroundValue(number, $scope.spread).toPrecision(numberOfSignificantFigures) + units;
 
       $scope.distractors.push({
         "i": i,
-        "value": $scope.randomNumberAroundValue(v, $scope.spread).toPrecision(sf) + value2[1]
+        "value": distractor
       });
     }
-
-
   }
+
+  $scope.generateDistractors();
 
 }]);
