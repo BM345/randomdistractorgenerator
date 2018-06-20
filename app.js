e5eb6fed28c6e1b64ddcc3f42a3a8f37e7592e88
Ben Milnes <benjamin.milnes@nagwa.com>
2018-06-20 16:00:28 +0100

    Improved styling a lot

--- app.js
+++ app.js
@@ -1,112 +1,89 @@
 var app = angular.module("RandomDistractorGenerator", []);
 
-app.controller("MainController", ["$scope", function MainController($scope){
+app.controller("MainController", ["$scope", function MainController($scope) {
 
-$scope.value = "10 m/s";
-$scope.spread = 0.5;
-$scope.numberOfDistractors = 4;
+  $scope.value = "10 m/s";
+  $scope.spread = 0.5;
+  $scope.numberOfDistractors = 4;
 
   $scope.distractors = [];
 
 
-$scope.randomNumberAroundValue = function(value, spread){
+  $scope.randomNumberAroundValue = function(value, spread) {
 
-var absoluteSpread = value * spread;
+    var absoluteSpread = value * spread;
 
-return value + 2 * absoluteSpread * Math.random() - absoluteSpread;
+    return value + 2 * absoluteSpread * Math.random() - absoluteSpread;
 
 
-}
-
-$scope.splitValueIntoNumberAndUnits = function(value){
-
-var splitPoint = -1;
-var s = value.toString();
-
-for (var i = 0; i < s.length; i++){
-  c = s[i];
-  if ("0123456789.".indexOf(c) == -1)
-  {
-    splitPoint = i;
-    break;
   }
-}
 
-console.log(splitPoint);
+  $scope.splitValueIntoNumberAndUnits = function(value) {
 
-return [s.slice(0, splitPoint), s.slice( splitPoint)];
+    var splitPoint = -1;
+    var s = value.toString();
 
-
-
-}
-
-$scope.getNumberOfSignificantFigures = function(numberString){
-
-var n = 0;
-var h = false;
-
-for (var i = 0; i < numberString.length; i++){
-var c = numberString[i];
-
-  if (h == false){
-    if ("123456789".indexOf(c) != -1){
-      h = true;
+    for (var i = 0; i < s.length; i++) {
+      c = s[i];
+      if ("0123456789.".indexOf(c) == -1) {
+        splitPoint = i;
+        break;
+      }
     }
-    else{
-      continue;
-    }
+
+    console.log(splitPoint);
+
+    return [s.slice(0, splitPoint), s.slice(splitPoint)];
+
+
+
   }
-  if (h == true){
-    if ( c != "."){
-      n ++;
-    }
-  }
-}
 
-return n;
+  $scope.getNumberOfSignificantFigures = function(number) {
 
-}
+    var n = 0;
+    var h = false;
+    var hasHadPoint = false;
+    var s = number.toString();
 
-$scope.roundToNSignificantFigures = function (value, n) {
-  var abs = Math.abs(value);
-  var sign = Math.sign(value);
-  var a = abs;
-  var y = 0;
+    for (var i = 0; i < s.length; i++) {
+      var c = s[i];
 
-
-    while (a < 1){
-      a = a * 10;
-      y++;
-    }
-
-        while (a >= 10){
-          a = a / 10;
-          y--;
+      if (h == false) {
+        if ("123456789".indexOf(c) != -1) {
+          h = true;
+        } else {
+          continue;
+        }
+      }
+      if (h == true) {
+        if (".".indexOf(c) == -1) {
+          n++;
         }
 
-        a = a * Math.pow(10, n);
-        a = Math.round(a);
-        a = a / Math.pow(10, n);
-        a = a * Math.pow(10, -y);
+      }
+    }
 
-        return sign *a;
+    return n;
+
+  }
 
 
+  $scope.generateDistractors = function() {
 
-}
+    var value2 = $scope.splitValueIntoNumberAndUnits($scope.value);
+    var v = parseFloat(value2[0])
+    var sf = parseInt($scope.getNumberOfSignificantFigures(value2[0].toString()));
 
-  $scope.generateDistractors = function () {
+    $scope.distractors = [];
 
-var value2 = $scope.splitValueIntoNumberAndUnits($scope.value);
-var v = parseFloat( value2[0])
-var sf = parseInt( $scope.getNumberOfSignificantFigures(value2[0].toString()));
+    for (var i = 0; i < $scope.numberOfDistractors; i++) {
 
-$scope.distractors = [];
-
-for (var i = 0; i < $scope.numberOfDistractors; i++){
-
-  $scope.distractors.push({"i":i,"value":  $scope.randomNumberAroundValue(v, $scope.spread).toPrecision( sf) + value2[1]});
-}
+      $scope.distractors.push({
+        "i": i,
+        "value": $scope.randomNumberAroundValue(v, $scope.spread).toPrecision(sf) + value2[1]
+      });
+    }
 
 
   }
